c804c585560efdca5106d96a9c00aa3a5742ec5b
Kevin Cheng <kevinphy@gmail.com>
2018-06-27 14:18:44 +0800

    Testing IK_server

--- kuka_arm/scripts/IK_server.py
+++ kuka_arm/scripts/IK_server.py
@@ -140,11 +140,11 @@ def handle_calculate_IK(req):
 		theta5 = atan2(sqrt(R3_6[0,2]**2 + R3_6[2,2]**2),R3_6[1,2])
 		
 		if (sin(theta5) > 0):
-		 	theta4 = atan2(-R3_6[2,2], R3_6[0,2])
-		 	theta6 = atan2(R3_6[1,1], -R3_6[1,0])
-		else:
 		 	theta4 = atan2(R3_6[2,2], -R3_6[0,2])
 		 	theta6 = atan2(-R3_6[1,1], R3_6[1,0])
+		else:
+		 	theta4 = atan2(-R3_6[2,2], R3_6[0,2])
+		 	theta6 = atan2(R3_6[1,1], -R3_6[1,0])
 
 		# Populate response for the IK request
 		# In the next line replace theta1,theta2...,theta6 by your joint angle variables
